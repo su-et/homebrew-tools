9e67c2faa073bbcc76eab4dd00a83d65aca00611
Scotty Logan <swl@stanford.edu>
2018-04-25 13:02:31 -0700

    update aws-tools to match source repo updates

--- Formula/aws-tools.rb
+++ Formula/aws-tools.rb
@@ -1,13 +1,16 @@
 class AwsTools < Formula
   desc "Emerging Technology AWS Tools"
   homepage "https://github.com/su-et/aws-tools/"
-  url "https://github.com/su-et/aws-tools/archive/v0.1.0.tar.gz"
-  sha256 "45a9986f343bd246181ca2cfa5307841044fc9f75439eb1572bfc906d3a70ac6"
+  url "https://github.com/su-et/aws-tools/archive/v0.1.1.tar.gz"
+  sha256 "6085add12cdfa41660f84ca6558eb9a2a4d9ebddfdf6d231402967b951fd2a7b"
 
   depends_on "awscli"
   depends_on "git-crypt"
   depends_on "jq"
+  depends_on "packer"
+  depends_on "s3cmd"
   depends_on "terraform"
+  depends_on "vault"
 
   def install
     bin.install Dir["bin/*"]
