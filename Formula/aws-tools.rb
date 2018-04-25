0b5a9623f247b08b7d649147491bc1538018e877
Scotty Logan <swl@stanford.edu>
2018-04-25 13:04:49 -0700

    fixed the hash for aws-tools v0.1.1

--- Formula/aws-tools.rb
+++ Formula/aws-tools.rb
@@ -2,7 +2,7 @@ class AwsTools < Formula
   desc "Emerging Technology AWS Tools"
   homepage "https://github.com/su-et/aws-tools/"
   url "https://github.com/su-et/aws-tools/archive/v0.1.1.tar.gz"
-  sha256 "6085add12cdfa41660f84ca6558eb9a2a4d9ebddfdf6d231402967b951fd2a7b"
+  sha256 "059270c5bd7950e0b35ceeab4d5b42486ca8dbc44b71ab225edc696551fbd254"
 
   depends_on "awscli"
   depends_on "git-crypt"
